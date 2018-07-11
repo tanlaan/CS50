611c37424bbbedefad72aaaf195b870b0c9356bc
Chris Bolas <tanlaan@users.noreply.github.com>
2018-07-11 05:21:23 +0000

    Added some much needed comments. Still a mess. But it passed the style check.

--- pset2/crack/crack.c
+++ pset2/crack/crack.c
@@ -9,17 +9,17 @@ void crack(string hash, char clear[]);
 
 int main(int argc, string argv[])
 {
-    if (argc != 2)
+    if (argc != 2)              // Failed to give correct amount of arguments
     {
         printf("Either too many or too few arguments. Example: ./crack 12345\n");
         return 1;
     }
     else
     {
-        string hash = argv[1];
-        char clear[6];
-        crack(hash, clear);
-        printf("%s\n", clear);
+        string hash = argv[1];  // Get hash string from command line argument
+        char clear[6];          // Cleartext variable
+        crack(hash, clear);     // Brute force cleartext
+        printf("%s\n", clear);  // Print it to screen
         return 0;
     }
 
@@ -27,25 +27,25 @@ int main(int argc, string argv[])
 
 void crack(string hash, char clear[])
 {
-    char alpha_chars[52];
-    int i, a;
-    for (i = 0, a = 65; i < 26; i++, a++)
+    char alpha_chars[52];                   // Array for all alphanumeric characters
+    int i, a;                               // Counter vars
+    for (i = 0, a = 65; i < 26; i++, a++)   // First loop through of capital leters
     {
         alpha_chars[i] = a;
     }
-    for (i = 26, a = 97; i < 52; i++, a++)
+    for (i = 26, a = 97; i < 52; i++, a++)  // Second loop through for lowercase letters
     {
         alpha_chars[i] = a;
     }
 
-    int depth = 0;
+    int depth = 0;                          // Set depth to zero to only start inner for loop
     for (i = 0; i < 52; i++)
     {
-        if (depth < 4)
+        if (depth < 4)  // Used to make sure that the "first run" that counts is the following run. i will equal 0 twice
         {
             i = -1;
         }
-        else
+        else            // Once we've passed through all the inner loops once we continue normally
         {
             clear[depth - 4] = alpha_chars[i];
         }
@@ -80,19 +80,19 @@ void crack(string hash, char clear[])
                     {
                         clear[depth - 1] = alpha_chars[l];
                     }
-                    for (int m = 0; m < 52; m++)
+                    for (int m = 0; m < 52; m++)                    // Inner most loop
                     {
-                        clear[depth] = alpha_chars[m];
-                        clear[depth + 1] = '\0';
-                        if (strcmp(crypt(clear, "50"), hash) == 0)
+                        clear[depth] = alpha_chars[m];              // First run depth = 0 and m = 0
+                        clear[depth + 1] = '\0';                    // End character in array is set to EOL
+                        if (strcmp(crypt(clear, "50"), hash) == 0)  // Compare strings of encrypted clear and original hash
                         {
-                            return;
+                            return;                                 // Success
                         }
 
                     }
-                    if (depth == 0)
+                    if (depth == 0)                                 // We must increase depth after our first run through to
                     {
-                        depth++;
+                        depth++;                                    // go from 1 character to 2
                     }
                 }
                 if (depth == 1)
